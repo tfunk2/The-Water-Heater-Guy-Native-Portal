f77433459f07a47f485e91def957086aed7fe409
tfunk2 <tyler.j.funk2@gmail.com>
2020-05-25 03:31:17 -0600

    Add delete button to Testimonials

--- components/ContactMessage.js
+++ components/ContactMessage.js
@@ -38,7 +38,7 @@ export default function ContactMessage (props) {
             <Text style={styles.innerText}>{props.item.email}</Text>
             <Text style={styles.innerText}>{props.item.phone_number}</Text>
             <Text style={styles.innerText}>{props.item.message}</Text>
-            <Button title="delete" onPress={() => props.deleteMessage(props.item.id)}/>
+            <Button title="Delete" onPress={() => props.deleteMessage(props.item.id)}/>
         </View>
     )
 }
